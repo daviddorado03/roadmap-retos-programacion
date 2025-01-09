2025ffe7a9fc2a6c1c043a1a87d24ca1da4da284
José David Dorado Treviño <38193853@uaz.edu.mx>
2025-01-09 15:12:10 -0600

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/daviddorado03.java
@@ -0,0 +1,23 @@
+public class daviddorado03 {
+
+    public static void main(String[] args) {
+        //https://www.java.com/es/
+        /**
+         * https://www.java.com/es/
+         * https://www.java.com/es/
+         */
+        var var1 = 1;
+        final var var2 = 3;
+
+        char char1 = 'a';
+        byte byte1 = 1;
+        short short1 = 12;
+        int int1 = 1234;
+        long long1 = 12345678;
+        double double1 = 1.1;
+        float float1 = 1.1f;
+        boolean boolean1 = true;
+
+        System.out.println("¡Hola, JAVA!");
+    }
+}
